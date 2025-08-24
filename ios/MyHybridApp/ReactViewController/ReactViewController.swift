133e0882f5d4fea54c8ccec8f483e76e8bd4b883
Gaurav Bisht <gaurav.bisht@tothenew.com>
2025-08-24 10:53:43 +0530

    Refactor ReactViewController to use dynamic moduleName for root view

--- ios/MyHybridApp/ReactViewController/ReactViewController.swift
+++ ios/MyHybridApp/ReactViewController/ReactViewController.swift
@@ -13,6 +13,16 @@ import ReactAppDependencyProvider
 class ReactViewController: UIViewController {
     var reactNativeFactory: RCTReactNativeFactory?
     var reactNativeFactoryDelegate: RCTReactNativeFactoryDelegate?
+    var moduleName: String
+    
+    init(moduleName: String) {
+        self.moduleName = moduleName
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -21,7 +31,7 @@ class ReactViewController: UIViewController {
         reactNativeFactoryDelegate.dependencyProvider = RCTAppDependencyProvider()
         reactNativeFactory = RCTReactNativeFactory(delegate: reactNativeFactoryDelegate)
         guard let reactNativeFactory else { return }
-        view = reactNativeFactory.rootViewFactory.view(withModuleName: "MyHybridApp")
+        view = reactNativeFactory.rootViewFactory.view(withModuleName: moduleName)
     }
 }
 
